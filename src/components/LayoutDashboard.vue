f1c41713e7c93aa9e9ddbaaf76fcca7d9bdc338f
aji sampurno <ajisampurno121@gmail.com>
2024-07-26 00:28:23 +0700

    update adminLTE

--- src/components/LayoutDashboard.vue
+++ src/components/LayoutDashboard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="/src/assets/AdminLTE-3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTELogo"
+        <img class="animation__shake" src="/src/assets/AdminLTE-3.2.0/dis/img/AdminLTELogo.png" alt="AdminLTELogo"
             height="60" width="60">
     </div>
 
@@ -51,7 +51,7 @@
                     <a href="#" class="dropdown-item">
 
                         <div class="media">
-                            <img src="/src/assets/AdminLTE-3.2.0/dist/img/user1-128x128.jpg" alt="User Avatar"
+                            <img src="/src/assets/AdminLTE-3.2.0/dis/img/user1-128x128.jpg" alt="User Avatar"
                                 class="img-size-50 mr-3 img-circle">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
@@ -68,7 +68,7 @@
                     <a href="#" class="dropdown-item">
 
                         <div class="media">
-                            <img src="/src/assets/AdminLTE-3.2.0/dist/img/user8-128x128.jpg" alt="User Avatar"
+                            <img src="/src/assets/AdminLTE-3.2.0/dis/img/user8-128x128.jpg" alt="User Avatar"
                                 class="img-size-50 img-circle mr-3">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
@@ -85,7 +85,7 @@
                     <a href="#" class="dropdown-item">
 
                         <div class="media">
-                            <img src="/src/assets/AdminLTE-3.2.0/dist/img/user3-128x128.jpg" alt="User Avatar"
+                            <img src="/src/assets/AdminLTE-3.2.0/dis/img/user3-128x128.jpg" alt="User Avatar"
                                 class="img-size-50 img-circle mr-3">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
@@ -147,7 +147,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
         <a href="#" class="brand-link">
-            <img src="/src/assets/AdminLTE-3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+            <img src="/src/assets/AdminLTE-3.2.0/dis/img/AdminLTELogo.png" alt="AdminLTE Logo"
                 class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
@@ -156,7 +156,7 @@
 
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="/src/assets/AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+                    <img src="/src/assets/AdminLTE-3.2.0/dis/img/user2-160x160.jpg" class="img-circle elevation-2"
                         alt="User Image">
                 </div>
                 <div class="info">
